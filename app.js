beb586fafe78e944f2fc4eb2c6fa57c404241169
Gedewon <gedewon.hayle.9@gmail.com>
2022-04-18 20:38:01 +0300

    Refactoring js

--- app.js
+++ app.js
@@ -1,34 +1,28 @@
 class BooksShelf {
   constructor() {
-    if (localStorage.getItem('books') === null) {
+    if (localStorage.getItem("books") === null) {
       this.books = [];
-    } else {
-      this.books = JSON.parse(localStorage.getItem('books'));
+      return;
     }
+    this.books = JSON.parse(localStorage.getItem("books"));
   }
 
-  addBook(book) {
+  addBook(book = null) {
+    if (book === null) return;
     this.books.push(book);
-
-    localStorage.setItem('books', JSON.stringify(this.books));
+    localStorage.setItem("books", JSON.stringify(this.books));
   }
 
   removeBook(bookIndex) {
-    this.books = this.books.filter((item, index) => {
-      if (index !== bookIndex) {
-        return item;
-      }
-      return undefined;
-    });
-
-    localStorage.setItem('books', JSON.stringify(this.books));
+    this.books = this.books.filter((item, index) => index !== bookIndex);
+    localStorage.setItem("books", JSON.stringify(this.books));
   }
 }
 
-const booksContainer = document.getElementById('books-dynamic-container');
-const addBookForm = document.getElementById('form-add-book');
-const titleInput = document.getElementById('title-input');
-const author = document.getElementById('author-input');
+const booksContainer = document.getElementById("books-dynamic-container");
+const addBookForm = document.getElementById("form-add-book");
+const titleInput = document.getElementById("title-input");
+const author = document.getElementById("author-input");
 // eslint-disable-next-line no-unused-vars
 
 const allAddedBooks = new BooksShelf();
@@ -38,30 +32,31 @@ function reload() {
     .map(
       (
         bookItem,
-        index,
+        index
       ) => `<div class="book-item" style="border-bottom: 1px black solid;" ><p><strong>${bookItem.titleInput}<br>${bookItem.author}.</strong></p>
         <button onclick="removeBook(${index})">Remove</button>
-        </div>`,
+        </div>`
     )
-    .join('');
+    .join("");
   if (allAddedBooks.books.length === 0) {
-    booksContainer.style.cssText = 'border: none;';
+    booksContainer.style.cssText = "border: none;";
   } else {
-    booksContainer.style.cssText = 'border-bottom: 2px black solid;';
+    booksContainer.style.cssText = "border-bottom: 2px black solid;";
   }
 }
 
 reload();
 
-addBookForm.addEventListener('submit', (event) => {
+addBookForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const newBook = {
     titleInput: titleInput.value,
     author: author.value,
   };
+
   allAddedBooks.addBook(newBook);
-  titleInput.value = '';
-  author.value = '';
+  titleInput.value = "";
+  author.value = "";
   reload();
 });
 
