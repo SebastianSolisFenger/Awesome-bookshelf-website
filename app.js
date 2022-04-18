7f64886afe6c383f8e731ff17d73ba3fd1a9f4dc
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-04-18 13:48:30 -0300

    Fix linter

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 class BooksShelf {
   constructor() {
-    if (localStorage.getItem("books") === null) {
+    if (localStorage.getItem('books') === null) {
       this.books = [];
     } else {
-      this.books = JSON.parse(localStorage.getItem("books"));
+      this.books = JSON.parse(localStorage.getItem('books'));
     }
   }
 
   addBook(book) {
     this.books.push(book);
 
-    localStorage.setItem("books", JSON.stringify(this.books));
+    localStorage.setItem('books', JSON.stringify(this.books));
   }
 
   removeBook(bookIndex) {
@@ -21,15 +21,15 @@ class BooksShelf {
       return undefined;
     });
 
-    localStorage.setItem("books", JSON.stringify(this.books));
+    localStorage.setItem('books', JSON.stringify(this.books));
   }
 }
 
-const booksContainer = document.getElementById("books-dynamic-container");
-const addBookForm = document.getElementById("form-add-book");
-const titleInput = document.getElementById("title-input");
-const author = document.getElementById("author-input");
-const bookItemClass = document.querySelectorAll("book-item");
+const booksContainer = document.getElementById('books-dynamic-container');
+const addBookForm = document.getElementById('form-add-book');
+const titleInput = document.getElementById('title-input');
+const author = document.getElementById('author-input');
+// eslint-disable-next-line no-unused-vars
 
 const allAddedBooks = new BooksShelf();
 
@@ -38,30 +38,30 @@ function reload() {
     .map(
       (
         bookItem,
-        index
+        index,
       ) => `<div class="book-item" style="border-bottom: 1px black solid;" ><p><strong>${bookItem.titleInput}<br>${bookItem.author}.</strong></p>
         <button onclick="removeBook(${index})">Remove</button>
-        </div>`
+        </div>`,
     )
-    .join("");
+    .join('');
   if (allAddedBooks.books.length === 0) {
-    booksContainer.style.cssText = "border: none;";
+    booksContainer.style.cssText = 'border: none;';
   } else {
-    booksContainer.style.cssText = "border-bottom: 2px black solid;";
+    booksContainer.style.cssText = 'border-bottom: 2px black solid;';
   }
 }
 
 reload();
 
-addBookForm.addEventListener("submit", (event) => {
+addBookForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const newBook = {
     titleInput: titleInput.value,
     author: author.value,
   };
   allAddedBooks.addBook(newBook);
-  titleInput.value = "";
-  author.value = "";
+  titleInput.value = '';
+  author.value = '';
   reload();
 });
 
