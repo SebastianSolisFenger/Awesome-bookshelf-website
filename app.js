66e24102618ec3ee8d693d63a89b3a8bee8b5dc3
Daniel Malo <danielmaloosorio@gmail.com>
2022-04-20 11:58:03 -0500

    Fix linter errors
    Fix linter errors in app.js.

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 class BooksShelf {
   constructor() {
-    if (localStorage.getItem("books") === null) {
+    if (localStorage.getItem('books') === null) {
       this.books = [];
     } else {
-      this.books = JSON.parse(localStorage.getItem("books"));
+      this.books = JSON.parse(localStorage.getItem('books'));
     }
   }
 
   addBook(book = null) {
     if (book === null) return;
     this.books.push(book);
-    localStorage.setItem("books", JSON.stringify(this.books));
+    localStorage.setItem('books', JSON.stringify(this.books));
   }
 
   removeBook(bookIndex) {
     this.books.splice(bookIndex, 1);
-    localStorage.setItem("books", JSON.stringify(this.books));
+    localStorage.setItem('books', JSON.stringify(this.books));
   }
 }
 
-const booksContainer = document.getElementById("books-dynamic-container");
-const addBookForm = document.getElementById("form-add-book");
-const titleInput = document.getElementById("title-input");
-const author = document.getElementById("author-input");
+const booksContainer = document.getElementById('books-dynamic-container');
+const addBookForm = document.getElementById('form-add-book');
+const titleInput = document.getElementById('title-input');
+const author = document.getElementById('author-input');
 // eslint-disable-next-line no-unused-vars
 
 const allAddedBooks = new BooksShelf();
@@ -32,20 +32,20 @@ function insertBooks() {
     .map(
       (
         bookItem,
-        index
+        index,
       ) => `<div class="book-item"  ><p class='title-author'><strong>"${bookItem.titleInput}" by ${bookItem.author}.</strong></p>
         <button class='remove-btn' onclick="removeBook(${index})">Remove</button>
-        </div>`
+        </div>`,
     )
-    .join("");
+    .join('');
   if (allAddedBooks.books.length === 0) {
-    booksContainer.style.cssText = "border: none;";
+    booksContainer.style.cssText = 'border: none;';
   }
 }
 
 insertBooks();
 
-addBookForm.addEventListener("submit", (event) => {
+addBookForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const newBook = {
     titleInput: titleInput.value,
@@ -53,8 +53,8 @@ addBookForm.addEventListener("submit", (event) => {
   };
 
   allAddedBooks.addBook(newBook);
-  titleInput.value = "";
-  author.value = "";
+  titleInput.value = '';
+  author.value = '';
   insertBooks();
 });
 
@@ -64,15 +64,15 @@ const removeBook = (bookIndex) => {
   insertBooks();
 };
 
-const sections = document.querySelectorAll(".section-con");
+const sections = document.querySelectorAll('.section-con');
 
 // eslint-disable-next-line no-unused-vars
 function displayActive(className) {
   sections.forEach((section) => {
     if (section.classList.contains(className)) {
-      section.classList.add("active");
+      section.classList.add('active');
     } else {
-      section.classList.remove("active");
+      section.classList.remove('active');
     }
   });
 }
